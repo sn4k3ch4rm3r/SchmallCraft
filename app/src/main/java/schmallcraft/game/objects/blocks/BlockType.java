3346babc72fee88f1a7c4d8d6b0d1412e49a28e4
Boldizsár Tóth <39193138+sn4k3ch4rm3r@users.noreply.github.com>
2023-11-22 21:56:52 +0100

    Remove DBI

--- app/src/main/java/schmallcraft/game/objects/blocks/BlockType.java
+++ app/src/main/java/schmallcraft/game/objects/blocks/BlockType.java
@@ -5,44 +5,38 @@ import java.util.HashMap;
 public enum BlockType {
 	GRASS, SAND, STONE, DIRT, TREE, WATER, ROCK, STAIR;
 
-	private static HashMap<Integer, BlockType> idToType = new HashMap<Integer, BlockType>() {
-		{
-			put(8, BlockType.GRASS);
-			put(42, BlockType.STONE);
-			put(41, BlockType.ROCK);
-			put(7, BlockType.TREE);
-			put(23, BlockType.SAND);
-			put(2, BlockType.WATER);
-			put(6, BlockType.GRASS);
-			put(37, BlockType.STAIR);
-		}
-	};
+	private static HashMap<Integer, BlockType> idToType = new HashMap<Integer, BlockType>();
+	private static HashMap<BlockType, BlockProperties> typeProperties = new HashMap<BlockType, BlockProperties>();
+	private static HashMap<BlockType, Integer> baseSpriteId = new HashMap<BlockType, Integer>();
 
-	private static HashMap<BlockType, BlockProperties> typeProperties = new HashMap<BlockType, BlockProperties>() {
-		{
-			put(BlockType.GRASS, new BlockProperties(1, 1));
-			put(BlockType.SAND, new BlockProperties(1, 1));
-			put(BlockType.STONE, new BlockProperties(1, 1));
-			put(BlockType.DIRT, new BlockProperties(1, 1));
-			put(BlockType.TREE, new BlockProperties(1, 1));
-			put(BlockType.WATER, new BlockProperties(-1, 0.5));
-			put(BlockType.ROCK, new BlockProperties(1, 1));
-			put(BlockType.STAIR, new BlockProperties(1, 1));
-		}
-	};
+	static {
+		idToType.put(8, BlockType.GRASS);
+		idToType.put(42, BlockType.STONE);
+		idToType.put(41, BlockType.ROCK);
+		idToType.put(7, BlockType.TREE);
+		idToType.put(23, BlockType.SAND);
+		idToType.put(2, BlockType.WATER);
+		idToType.put(6, BlockType.GRASS);
+		idToType.put(37, BlockType.STAIR);
 
-	private static HashMap<BlockType, Integer> baseSpriteId = new HashMap<BlockType, Integer>() {
-		{
-			put(BlockType.GRASS, 0x00);
-			put(BlockType.STONE, 0x01);
-			put(BlockType.TREE, 0x02);
-			put(BlockType.SAND, 0x03);
-			put(BlockType.ROCK, 0x04);
-			put(BlockType.WATER, 0x05);
-			put(BlockType.DIRT, 0x06);
-			put(BlockType.STAIR, 0x07);
-		}
-	};
+		typeProperties.put(BlockType.GRASS, new BlockProperties(1, 1));
+		typeProperties.put(BlockType.SAND, new BlockProperties(1, 1));
+		typeProperties.put(BlockType.STONE, new BlockProperties(1, 1));
+		typeProperties.put(BlockType.DIRT, new BlockProperties(1, 1));
+		typeProperties.put(BlockType.TREE, new BlockProperties(1, 1));
+		typeProperties.put(BlockType.WATER, new BlockProperties(-1, 0.5));
+		typeProperties.put(BlockType.ROCK, new BlockProperties(1, 1));
+		typeProperties.put(BlockType.STAIR, new BlockProperties(1, 1));
+
+		baseSpriteId.put(BlockType.GRASS, 0x00);
+		baseSpriteId.put(BlockType.STONE, 0x01);
+		baseSpriteId.put(BlockType.TREE, 0x02);
+		baseSpriteId.put(BlockType.SAND, 0x03);
+		baseSpriteId.put(BlockType.ROCK, 0x04);
+		baseSpriteId.put(BlockType.WATER, 0x05);
+		baseSpriteId.put(BlockType.DIRT, 0x06);
+		baseSpriteId.put(BlockType.STAIR, 0x07);
+	}
 
 	public int baseSpriteId() {
 		return baseSpriteId.get(this);
