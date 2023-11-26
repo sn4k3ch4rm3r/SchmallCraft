10673360e9c4865a4a7de8fd489d9de257cc5636
Boldizsár Tóth <39193138+sn4k3ch4rm3r@users.noreply.github.com>
2023-11-26 13:20:45 +0100

    Make the pig wander around sometimes

--- app/src/main/java/schmallcraft/game/objects/entities/Pig.java
+++ app/src/main/java/schmallcraft/game/objects/entities/Pig.java
@@ -4,13 +4,17 @@ import schmallcraft.util.RectangleD;
 import schmallcraft.util.Vector2;
 import static schmallcraft.util.Constants.TILE_SIZE;
 
+import schmallcraft.game.Game;
 import schmallcraft.items.ItemType;
 
 public class Pig extends Entity {
+	public static final double MOVEMENT_SPEED = 1.5;
+	private Vector2 target;
 
 	public Pig(Vector2 position) {
 		super(position, 5);
 		getDropTable().setDropRate(ItemType.RAW_PORK, 1.5);
+		target = position;
 	}
 
 	@Override
@@ -22,8 +26,27 @@ public class Pig extends Entity {
 	}
 
 	@Override
+	public void fixedUpdate() {
+		if (Game.random.nextInt(100) == 0) {
+			Vector2 offset = new Vector2(Game.random.nextDouble() * 10 - 5, Game.random.nextDouble() * 10 - 5);
+			target = getPosition().add(offset);
+		}
+	}
+
+	@Override
+	public void update(double deltaTime) {
+		Vector2 direction = target.subtract(getPosition());
+		if (direction.magnitude() > 0.1) {
+			setVelocity(direction.normalize().multiply(MOVEMENT_SPEED));
+		} else {
+			setVelocity(new Vector2());
+		}
+		super.update(deltaTime);
+	}
+
+	@Override
 	public int getSpriteId() {
-		return 0x14;
+		return 0x14 * (isFlipped() ? -1 : 1);
 	}
 
 	@Override
